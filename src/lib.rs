2d51ec82143b77f6943eed1489a7c36abb0f4cf5
Max Thomson <git@mnt.dev>
2024-07-17 22:43:52 -0700

    Basic implementation on top of leptops

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+pub use leptos::{component, view, IntoView};
 
+#[macro_export]
+macro_rules! html {
+    ($($view:tt)*) => {{
+        leptos::ssr::render_to_string(|| view! { $($view)* }).to_string()
+    }};
+}
